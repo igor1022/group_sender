8e3784b1011fca514c4d73cee4af0b48e623d291
Igor <imitroshichev@gmail.com>
2024-03-03 15:36:56 +0200

    second

--- index copy.js
+++ index copy.js
@@ -1,11 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Укажите свой токен, полученный у BotFather
-const token = '6382547749:AAEJHcWuTu_y3EUS_yAK4xa6Dwt8FF95saM';
+const token = '';
 
 // ID групп, куда будут пересылаться сообщения
-const group1ChatId = -1002102662359;
-const group2ChatId = -1002055395865;
+const group1ChatId = -1002046062164;
+const group2ChatId = -1002088693739;
+const group3ChatId = -1001987266452;
+const group4ChatId = -1001920113346;
+const group5ChatId = -1002029904846;
 
 // Создаем бота
 const bot = new TelegramBot(token, { polling: true });
@@ -13,10 +16,9 @@ const bot = new TelegramBot(token, { polling: true });
 // Обработчик сообщений
 bot.on('message', (msg) => {
     const messageText = msg.text;
-
-    // Пересылаем сообщение в первую группу
     bot.sendMessage(group1ChatId, messageText);
-
-    // Пересылаем сообщение во вторую группу
     bot.sendMessage(group2ChatId, messageText);
+    bot.sendMessage(group3ChatId, messageText);
+    bot.sendMessage(group4ChatId, messageText);
+    bot.sendMessage(group5ChatId, messageText);
 });
